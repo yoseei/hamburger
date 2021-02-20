68f572fc3b0f51616b90798bd358766d25e631f1
甲斐義隆 <katashiyoika@mba.local>
2021-02-20 13:51:44 +0900

    complete stalker animation

--- js/main.js
+++ js/main.js
@@ -11,10 +11,40 @@
     hamburger();
   });
   //マウスストーカー用のdivを取得
-  const stalker = document.getElementById('stalker'); 
+  const stalker = document.getElementById('stalker');
 
-  //上記のdivタグをマウスに追従させる処理
+  //aタグにホバー中かどうかの判別フラグ
+  let hovFlag = false;
+
+  //マウスに追従させる処理 （リンクに吸い付いてる時は除外する）
   document.addEventListener('mousemove', function (e) {
+      if (!hovFlag) {
       stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+      }
   });
+
+  //リンクへ吸い付く処理
+  const linkElem = document.querySelectorAll('a:not(.no_stick_)');
+  for (let i = 0; i < linkElem.length; i++) {
+      //マウスホバー時
+      linkElem[i].addEventListener('mouseover', function (e) {
+          hovFlag = true;
+
+          //マウスストーカーにクラスをつける
+          stalker.classList.add('hov_');
+
+          //マウスストーカーの位置をリンクの中心に固定
+          let rect = e.target.getBoundingClientRect();
+          let posX = rect.left + (rect.width / 2);
+          let posY = rect.top + (rect.height / 2);
+
+          stalker.style.transform = 'translate(' + posX + 'px, ' + posY + 'px)';
+
+      });
+      //マウスホバー解除時
+      linkElem[i].addEventListener('mouseout', function (e) {
+          hovFlag = false;
+          stalker.classList.remove('hov_');
+      });
+  }
 }
