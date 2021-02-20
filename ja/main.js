850a584c8a5cdf8b03e15de814644ab5902c53e7
甲斐義隆 <katashiyoika@mba.local>
2021-02-20 10:41:31 +0900

    first commit

---
+++ ja/main.js
@@ -0,0 +1,12 @@
+'use strict';
+{
+  function hamburger() {
+    document.getElementById('line1').classList.toggle('line_1');
+    document.getElementById('line2').classList.toggle('line_2');
+    document.getElementById('line3').classList.toggle('line_3');
+    document.getElementById('ham-nav').classList.toggle('in');
+  }
+  document.getElementById('ham-line').addEventListener('click', function() {
+    hamburger();
+  });
+}
