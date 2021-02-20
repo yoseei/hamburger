8e8bea50fb59a14fd7baa60e29ec3cf71b09c05f
甲斐義隆 <katashiyoika@mba.local>
2021-02-20 13:24:43 +0900

    add stalker

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 {
+  
   function hamburger() {
     document.getElementById('line1').classList.toggle('line_1');
     document.getElementById('line2').classList.toggle('line_2');
@@ -8,5 +9,12 @@
   }
   document.getElementById('ham-line').addEventListener('click', function() {
     hamburger();
+  });
+  //マウスストーカー用のdivを取得
+  const stalker = document.getElementById('stalker'); 
+
+  //上記のdivタグをマウスに追従させる処理
+  document.addEventListener('mousemove', function (e) {
+      stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
   });
 }
